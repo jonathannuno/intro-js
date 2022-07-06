fd276c1caacbc2b7d2d3fbc99bfa3489b18dd205
jonathannuno <jonathannunojarquin@gmail.com>
2022-07-05 21:33:45 -0500

    ejercicios de JS

---
+++ scripts.js
@@ -0,0 +1,76 @@
+/* alert("hola");
+
+document.write("JS");
+
+console.log("esto es texto", prompt("¿Como te llamas?")); */
+
+/* Tpos de datos Primitivos
+
+    Strings - (cadenas de caracteres)
+    numbers - (numeros)
+    booleans - (verdadero o falso)
+    
+    Null
+    Undefined
+
+    Symbols - (Simbolo)
+
+    Datos Prototipos
+
+    Array []
+
+    Object {}
+
+ */
+
+    // Variables
+
+   /*  var nombreDeLaVariable;
+
+    var primerNombre = "Jona";
+
+    console.log(primerNombre); */
+
+    // declara una variable
+
+var comida;
+    
+
+    /* console.log("line 2 ", comida); */
+
+    // declarar y asignar una variable
+
+    /* var pantalla = "Samsung";
+
+    console.log(pantalla); */
+
+    //  asignar el valor a la primer variable
+
+    comida = "Pozole";
+
+    console.log(comida);
+
+    // Declara una variable por cada tipo de dato
+
+    var dulce = "chicle";
+
+    console.log(dulce);
+
+    var numero = 2;
+
+    console.log(numero);
+
+    var pregunta = true;
+
+    console.log(pregunta);
+
+    // imprime en consola una suma
+
+    console.log(1 + 2);
+
+    // Declara y asigna dos variables y numericas e imprime en consola la suma de esas variables
+
+    var num1 = 2;
+    var num2 = 10;
+
+    console.log(num1 + num2);
